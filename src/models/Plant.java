0bf7db1aacc46567a4554d558e1843fb66d5c83c
ali.masooman@gmail.com <ali.masooman@gmail.com>
2025-04-13 15:06:37 +0330

    a little plant development

--- src/models/Plant.java
+++ src/models/Plant.java
@@ -3,15 +3,58 @@ package models;
 import enums.PlantsName;
 import enums.Season;
 
+import java.util.ArrayList;
+
 public class Plant {
+
+    private boolean isVegetable;
+    private int plantLevel;
+    private int daysToNextLevel;
+    private ArrayList<Integer> stages;
+    private int baseSellPrice;
     private PlantsName name;
-    private Seed Source;
-    private int totalHarvestTime;
+    private Seed source;
     private boolean oneTime;
     private int regrowthTime;
     private boolean isEdible;
     private int baseEnergy;
     private int baseHealth;
-    private Season season;
+    private ArrayList<Season> seasons;
     private boolean canBecomeGiant;
+
+    public Plant(
+            boolean isVegetable,
+            int plantLevel,
+            int daysToNextLevel,
+            ArrayList<Integer> stages,
+            int baseSellPrice,
+            PlantsName name,
+            Seed source,
+            boolean oneTime,
+            int regrowthTime,
+            boolean isEdible,
+            int baseEnergy,
+            int baseHealth,
+            ArrayList<Season> seasons,
+            boolean canBecomeGiant) {
+        this.isVegetable = isVegetable;
+        this.plantLevel = plantLevel;
+        this.daysToNextLevel = daysToNextLevel;
+        this.stages = stages;
+        this.baseSellPrice = baseSellPrice;
+        this.name = name;
+        this.source = source;
+        this.oneTime = oneTime;
+        this.regrowthTime = regrowthTime;
+        this.isEdible = isEdible;
+        this.baseEnergy = baseEnergy;
+        this.baseHealth = baseHealth;
+        this.seasons = seasons;
+        this.canBecomeGiant = canBecomeGiant;
+
+    }
+
+/*    public static Plant makePlant() {
+
+    }*/
 }
